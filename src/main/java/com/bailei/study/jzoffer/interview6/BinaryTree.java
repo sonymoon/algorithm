b13b846f08bff34094282d52e0a9d69e99db5a34
bailei <bailei@jk.cn>
2016-12-04 22:21:06 +0800

    改进重建二叉树，原地重建

--- src/main/java/com/bailei/study/jzoffer/interview6/BinaryTree.java
+++ src/main/java/com/bailei/study/jzoffer/interview6/BinaryTree.java
@@ -7,17 +7,21 @@ package com.bailei.study.jzoffer.interview6;
 public class BinaryTree {
 
 
-    public BinaryTreeNode reconstruct(int[] preOrder, int[] inOrder) {
-        if (preOrder.length == 0 || inOrder.length == 0) {
+    public BinaryTreeNode reconstruct(int[] preOrder, int[] inOrder,
+                                      int startPre, int endPre,
+                                      int startIn, int endIn) {
+        int lengthPre = endPre - startPre + 1;
+        int lengthIn = endIn - startIn +1;
+        if (lengthPre == 0 || lengthIn == 0) {
             return null;
         }
-        if (preOrder.length == 1 || inOrder.length == 1) {
-            return new BinaryTreeNode(preOrder[0]);
+        if (lengthPre == 1 || lengthIn == 1) {
+            return new BinaryTreeNode(preOrder[startPre]);
         }
-        int rootValue = preOrder[0];
+        int rootValue = preOrder[startPre];
         int rootValueIndexOfInOrder = -1;
 
-        for (int i = 0; i < inOrder.length; i++) {
+        for (int i = startIn; i <= endIn; i++) {
             if (rootValue == inOrder[i]) {
                 rootValueIndexOfInOrder = i;
                 break;
@@ -27,30 +31,22 @@ public class BinaryTree {
             throw new RuntimeException("不能重建二叉树，序列不匹配");
         }
 
-        //leftTreeOrder & rightTreeOrder
-        int leftTreeLength = rootValueIndexOfInOrder;
-        int rightTreeLength = preOrder.length - 1 - leftTreeLength;
-        int[] leftTreePreOrder = new int[leftTreeLength];
-        int[] rightTreePreOrder = new int[rightTreeLength];
-        int[] leftTreeInOrder = new int[leftTreeLength];
-        int[] rightTreeInOrder = new int[rightTreeLength];
-        if (leftTreeLength > 0) {
-            for (int i = 0; i < leftTreeLength; ++i) {
-                leftTreePreOrder[i] = preOrder[i + 1];
-                leftTreeInOrder[i] = inOrder[i];
-            }
-        }
-        if (rightTreeLength > 0) {
-            for (int i = 0; i < rightTreeLength; ++i) {
-                rightTreePreOrder[i] = preOrder[rootValueIndexOfInOrder + 1 + i];
-                rightTreeInOrder[i] = inOrder[rootValueIndexOfInOrder + 1 + i];
-            }
-        }
+        int leftLength = rootValueIndexOfInOrder - startIn;
+        int rightLength = endIn - rootValueIndexOfInOrder;
 
         BinaryTreeNode rootNode = new BinaryTreeNode(rootValue);
+        if (leftLength > 0) {
+            rootNode.pLeft = reconstruct(preOrder, inOrder,
+                    startPre + 1, startPre + leftLength,
+                    startIn, startIn + leftLength -1);
+        }
 
-        rootNode.pLeft = reconstruct(leftTreePreOrder, leftTreeInOrder);
-        rootNode.pRight = reconstruct(rightTreePreOrder, rightTreeInOrder);
+        if (rightLength > 0) {
+            rootNode.pRight = reconstruct(preOrder, inOrder,
+                    startPre + leftLength + 1, startPre + leftLength + rightLength,
+                    rootValueIndexOfInOrder + 1, rootValueIndexOfInOrder + rightLength);
+
+        }
 
         return rootNode;
     }
@@ -60,19 +56,25 @@ public class BinaryTree {
         int[] preOrder = {1, 2, 4, 7, 3, 5, 6, 8};
         int[] inOrder = {4, 7, 2, 1, 5, 3, 8, 6};
         BinaryTree binaryTree = new BinaryTree();
-        BinaryTreeNode root = binaryTree.reconstruct(preOrder, inOrder);
+        BinaryTreeNode root = binaryTree.reconstruct(preOrder, inOrder,
+                0, preOrder.length - 1,
+                0,inOrder.length - 1);
         System.out.println(root);
 
         //没有右子结点
         int[] preOrder1 = {1, 2, 3, 4, 5};
         int[] inOrder1 = {5, 4, 3, 2, 1};
-        root = binaryTree.reconstruct(preOrder1, inOrder1);
+         root = binaryTree.reconstruct(preOrder1, inOrder1,
+                0, preOrder1.length - 1,
+                0,inOrder1.length - 1);
         System.out.println(root);
 
         //没有左子结点
         int[] preOrder2 = {1, 2, 3, 4, 5};
         int[] inOrder2 = {1, 2, 3, 4, 5};
-        root = binaryTree.reconstruct(preOrder2, inOrder2);
+        root = binaryTree.reconstruct(preOrder2, inOrder2,
+                0, preOrder2.length - 1,
+                0,inOrder2.length - 1);
         System.out.println(root);
 
     }
