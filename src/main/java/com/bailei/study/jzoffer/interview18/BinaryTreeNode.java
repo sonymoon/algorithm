591d09ce1144c22258aea5e8bfe332b3a1d4137a
bailei <bailei@jk.cn>
2016-12-11 21:43:39 +0800

    单例的几种方式

---
+++ src/main/java/com/bailei/study/jzoffer/interview18/BinaryTreeNode.java
@@ -0,0 +1,15 @@
+package com.bailei.study.jzoffer.interview18;
+
+/**
+ * Created by bailei on 16/12/11.
+ */
+public class BinaryTreeNode {
+    int key;
+    BinaryTreeNode left, right;
+
+    public BinaryTreeNode(int key) {
+        this.key = key;
+    }
+
+
+}
--- src/main/java/com/bailei/study/jzoffer/interview2/package-info.java
+++ src/main/java/com/bailei/study/jzoffer/interview2/package-info.java
@@ -1,5 +1,5 @@
 /**
  * Created by bailei on 16/12/3.
- * 剑指Offer面试题2 java 实现
+ * 剑指Offer面试题2 java 实现: 单例的几种方式
  */
 package com.bailei.study.jzoffer.interview2;
