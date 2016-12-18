f5096045d1e5504b4dbbe85322ea3d0ba1402c6b
bailei <bailei@jk.cn>
2016-12-18 23:21:45 +0800

    interview26 复制复杂链表

--- src/main/java/com/bailei/study/jzoffer/interview26/ComplexListClone.java
+++ src/main/java/com/bailei/study/jzoffer/interview26/ComplexListClone.java
@@ -28,11 +28,66 @@ public class ComplexListClone {
         return copy;
     }
 
+    /**
+     * 原地复制
+     * 1、将复制结点放在原来链表后面
+     * 2、设置sibling
+     * 3、拆分两个链表
+     *
+     * @param source
+     * @return
+     */
+    public ComplexListNode clone(ComplexListNode source) {
+        if (source == null) {
+            return null;
+        }
+        cloneNodes(source);
+        cloneSiblings(source);
+        return partionList(source);
+    }
+
+    private void cloneNodes(ComplexListNode head) {
+        ComplexListNode p = head;
+        while (p != null) {
+            ComplexListNode pCopy = new ComplexListNode(p.value);
+            pCopy.next = p.next;
+            p.next = pCopy;
+            p = pCopy.next;
+        }
+    }
+
+    private void cloneSiblings(ComplexListNode head) {
+        ComplexListNode pCopy = head.next, p = head;
+        while (p != null) {
+            if (p.sibling != null) {
+                pCopy.sibling = p.sibling.next;
+            }
+            p = pCopy.next;
+            pCopy = p == null ? null : p.next;
+        }
+    }
+
+    private ComplexListNode partionList(ComplexListNode head) {
+        ComplexListNode copyHead = head.next;
+        ComplexListNode pCopy, p = head;
+        while (p != null) {
+            pCopy = p.next;
+            p.next = pCopy.next;
+            pCopy.next = pCopy.next == null ? null : pCopy.next.next;
+            p = p.next;
+        }
+        return copyHead;
+    }
+
+
     public static void main(String[] args) {
         ComplexListNode head = new ComplexListNode(new int[]{1, 2, 3, 4, 5, 6, 7, 8},
-                new int[]{0, 2},
-                new int[]{3, 7},
+                new int[]{0, 1},
+                new int[]{1, 0},
                 new int[]{7, 4});
-        Utils.printList(new ComplexListClone().cloneUsingHashTable(head));
+        Utils.printList(new ComplexListClone().clone(head));
+        head = new ComplexListNode(new int[]{1},
+                new int[]{0, 0});
+        Utils.printList(new ComplexListClone().clone(null));
     }
 }
