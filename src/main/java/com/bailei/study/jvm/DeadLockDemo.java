f1273a6ccb689df89a960fc2a16c9612bfcffc27
bailei <bailei@jk.cn>
2016-12-23 21:47:05 +0800

    死锁demo

---
+++ src/main/java/com/bailei/study/jvm/DeadLockDemo.java
@@ -0,0 +1,38 @@
+package com.bailei.study.jvm;
+
+/**
+ * Created by bailei on 16/12/23.
+ */
+public class DeadLockDemo {
+
+    static class SynAddRunable implements Runnable {
+        int a, b;
+
+        public SynAddRunable(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        @Override
+        public void run() {
+            synchronized (Integer.valueOf(a)) {
+                try {
+                    Thread.sleep(50L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (Integer.valueOf(b)) {
+                    System.out.println(a + b);
+                }
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            new Thread(new SynAddRunable(1, 2)).start();
+            new Thread(new SynAddRunable(2, 1)).start();
+        }
+    }
+}
