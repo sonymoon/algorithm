95648643c285649395826d7e3034f772613402ed
bailei <bailei@jk.cn>
2017-01-13 23:23:27 +0800

    玩玩zookeeper

---
+++ src/main/java/com/bailei/study/zookeeper/ZooConfig.java
@@ -0,0 +1,11 @@
+package com.bailei.study.zookeeper;
+
+/**
+ * Created by bailei on 17/1/13.
+ */
+public class ZooConfig {
+    public static final String IP = "10.255.136.58";
+    public static final int PORT = 2181;
+
+
+}
