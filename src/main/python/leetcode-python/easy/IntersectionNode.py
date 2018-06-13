10f65877c7d4ab58b283dd702888392a31c3061d
白磊 <bailei02@meituan.com>
2018-06-13 13:37:23 +0800

    leetCode160

--- src/main/python/leetcode-python/easy/IntersectionNode.py
+++ src/main/python/leetcode-python/easy/IntersectionNode.py
@@ -1,44 +1,18 @@
 # Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-
-class Solution(object):
-    def getIntersectionNode(self, head1, head2):
-        """
-        :type head1, head2: ListNode
-        :rtype: ListNode
-        """
-        if None in (head1, head2):
+class Solution:
+    # @param two ListNodes
+    # @return the intersected ListNode
+    def getIntersectionNode(self, headA, headB):
+        if headA is None or headB is None:
             return None
-        p1 = head1
-        p2 = head2
-        if p1 == p2:
-            return head1
 
-        tail1 = None
-        tail2 = None
-        while p1:
-            tail1 = p1
-            p1 = p1.next
-        while p2:
-            tail2 = p2
-            p2 = p2.next
-        if tail1 and tail2 and tail1 == tail2:
-            return tail1
-        if tail1 and tail2 and tail1 != tail2:
-            return None
-        p1 = head1
-        p2 = head2
-        while p1.val != p2.val:
-            if p1.next == None:
-                p1.next = head2
-            if p2.next == None:
-                p2.next = head1
-            p1 = p1.next
-            p2 = p2.next
-        tail1.next = None
-        tail2.next = None
-        return p1
+        pa = headA # 2 pointers
+        pb = headB
+
+        while pa is not pb:
+            # if either pointer hits the end, switch head and continue the second traversal, 
+            # if not hit the end, just move on to next
+            pa = headB if pa is None else pa.next
+            pb = headA if pb is None else pb.next
+
+        return pa # only 2 ways to get out of the loop, they meet or the both hit the end=None
