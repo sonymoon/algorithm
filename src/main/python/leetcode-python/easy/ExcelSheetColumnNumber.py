72822bae6c9d7e191ea191238b7b6f5492859a33
白磊 <bailei02@meituan.com>
2018-06-14 13:24:53 +0800

    leetcode 172

---
+++ src/main/python/leetcode-python/easy/ExcelSheetColumnNumber.py
@@ -0,0 +1,13 @@
+class Solution:
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        sum = 0
+        for char in s:
+            sum = sum * 26 + (ord(char) - 64)
+        return sum
+
+
+print(Solution().titleToNumber("AB"))
