a3d3452ee81f4f2322b4879759a4b695170ba9e1
白磊 <bailei02@meituan.com>
2019-02-14 23:37:51 +0800

    dfs

---
+++ src/main/python/geeksforgeeks/DFS.py
@@ -0,0 +1,46 @@
+from collections import defaultdict
+
+
+class Graph:
+
+    # Constructor
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+
+    def DFS(self):
+        visited = [False] * (max(self.graph.keys()) + 1)
+        queue = []
+        for u in self.graph.keys():
+            if not visited[u]:
+                queue.append(u)
+            while queue:
+                s = queue.pop(0)
+                print(s, end='')
+                for i in self.graph[s]:
+                    if not visited[i]:
+                        queue.append(i)
+                        visited[i] = True
+                        break;
+
+
+# Driver code
+
+# Create a graph given in
+# the above diagram
+g = Graph()
+g.addEdge(0, 1)
+g.addEdge(0, 2)
+g.addEdge(1, 2)
+g.addEdge(2, 0)
+g.addEdge(2, 3)
+g.addEdge(3, 3)
+g.addEdge(3, 6)
+g.addEdge(6, 6)
+
+print("Following is Breadth First Traversal"
+      " (starting from vertex 2)")
+print('graph length ', len(g.graph))
+g.DFS()
