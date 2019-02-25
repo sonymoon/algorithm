d5dab6aeec47e41772bb76c306ab109a4130ce93
白磊 <bailei02@meituan.com>
2019-02-25 20:26:28 +0800

    quick sort pick first element as pivot

--- src/main/python/geeksforgeeks/search-sorting/quick-sort.py
+++ src/main/python/geeksforgeeks/search-sorting/quick-sort.py
@@ -11,11 +11,22 @@ def partition(arr, low, high):
     return i
 
 
+def partition_first(arr, low, high):
+    pivot = arr[low]
+    i = high  # point to smaller one in the right part
+    for j in range(high, low, -1):  # j point to the bigger than pivot one in the left part
+        if arr[j] >= pivot:
+            arr[j], arr[i] = arr[i], arr[j]
+            i -= 1
+    arr[low], arr[i] = arr[i], arr[low]
+    return i
+
+
 def quick_sort(arr, low, high):
     if low >= high:
         return
 
-    pivot = partition(arr, low, high)
+    pivot = partition_first(arr, low, high)
 
     quick_sort(arr, low, pivot - 1)
     quick_sort(arr, pivot + 1, high)
