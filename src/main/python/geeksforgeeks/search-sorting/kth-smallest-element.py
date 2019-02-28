c5659e9bafaa8506f66a7f7b5ebf743a1c1eabe9
白磊 <bailei02@meituan.com>
2019-02-28 20:26:22 +0800

    kth

---
+++ src/main/python/geeksforgeeks/search-sorting/kth-smallest-element.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+
+def partition(arr, l, r):
+    if l > r:
+        return
+    pivot = arr[r]
+
+    i, j = l, l
+
+    while i <= r - 1 and j <= r - 1:
+
+        if arr[j] < pivot:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+        j += 1
+    arr[i], arr[j] = arr[j], arr[i]
+    return i
+
+
+def find_kth_smallest(arr, k, l, r):
+    if l > r or k <= 0:
+        return None
+
+    p = partition(arr, l, r)
+
+    if p - l == k - 1:
+        return arr[p]
+    elif p - l > k - 1:  # 则第k小的在左边
+        return find_kth_smallest(arr, k, l, p - 1)
+    else:
+        return find_kth_smallest(arr, k - (p - l), p + 1, r)
+
+
+if __name__ == "__main__":
+    arr = [7, 10, 4, 3, 20, 15]
+    k = 4
+    print find_kth_smallest(arr, k, 0, len(arr) - 1)
