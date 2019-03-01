d99d76d8aa7e1ef56882450e70e2c14f1378ac72
白磊 <bailei02@meituan.com>
2019-03-01 15:55:48 +0800

    fix

---
+++ src/main/python/geeksforgeeks/is-tree-full.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import sys
+
+# A full binary tree is defined as a binary tree in which all nodes have either zero or two child nodes. Conversely,
+# there is no node in a full binary tree, which has one child node. More information about full binary trees can be found here.
+
+
+def is_tree_full(root):
+    pass
